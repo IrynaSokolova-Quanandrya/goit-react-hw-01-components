012ffc66070746e63640022216822654db0b587b
Iryna <irasokolova25@ukr.net>
2021-10-26 23:50:50 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import "./App.css";
-import Profile from "./components/Profile/Profile";
-import user from "./components/Profile/user.json";
+// import Profile from "./components/Profile/Profile";
+// import user from "./components/Profile/profile-user.json";
+// import Statistics from './components/Statistics/Statistics';
+// import statisticalData from "./components/Statistics/statistical-data.json";
+// import friends from './components/FriendList/friends.json';
 
 export default function App() {
   return (
     <div className="App">
-      <Profile
+      {/* <Profile
+      key = {user.tag}
         name={user.name}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-      />
+      /> */}
+      {/* {statisticalData.map(obj => {
+          return <Statistics title="Upload stats" stats={statisticalData} />;
+        }
+      )} */}
+
+      {/* <FriendList friends={friends} />, */}
     </div>
   );
 }
