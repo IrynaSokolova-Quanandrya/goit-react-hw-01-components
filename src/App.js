281db349347240826461742b61b5bcf6d3fa240f
Iryna <irasokolova25@ukr.net>
2021-10-27 18:12:13 +0300

    добавила рендер по условию и мепнула массив

--- src/App.js
+++ src/App.js
@@ -3,24 +3,21 @@ import Profile from "./components/Profile/Profile";
 import user from "./components/Profile/profile-user.json";
 import Statistics from "./components/Statistics/Statistics";
 import statisticalData from "./components/Statistics/statistical-data.json";
-import friends from "./components/FriendList/friends.json";
-
+// import friends from "./components/FriendList/friends.json";
+console.log(statisticalData);
 export default function App() {
   return (
     <div className="App">
-      <Profile
+      {/* <Profile
         key={user.tag}
         name={user.name}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-      />
-      {/* {statisticalData.map(obj => {
-          return <Statistics title="Upload stats" stats={statisticalData} />;
-        }
-      )} */}
-
+      /> */}
+      <Statistics title="Upload stats" stats={statisticalData} />;
+      <Statistics stats={statisticalData} />;
       {/* <FriendList friends={friends} />, */}
     </div>
   );
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,27 +1,34 @@
-function Statistics({ id, label, percentage }) {
+import PropTypes from "prop-types";
+
+function Statistics({ title, stats }) {
   return (
     <section className="statistics">
-      <h2 className="title">Upload stats</h2>
+      {{ title } && <h2 className="title">{title}</h2>}
 
       <ul className="stat-list">
-        <li className="item" key={id}>
-          <span className="label">{label}</span>
-          <span className="percentage">{percentage}</span>
-        </li>
-        <li className="item" key={id}>
-          <span className="label">{label}</span>
-          <span className="percentage">{percentage}</span>
-        </li>
-        <li className="item" key={id}>
-          <span className="label">{label}</span>
-          <span className="percentage">{percentage}</span>
-        </li>
-        <li className="item" key={id}>
-          <span className="label">{label}</span>
-          <span className="percentage">{percentage}</span>
-        </li>
+        {stats.map((stat) => (
+          <li className="item" key={stat.id}>
+            <span className="label">{stat.label}</span>
+            <span className="percentage">{stat.percentage}</span>
+          </li>
+        ))}
       </ul>
     </section>
   );
 }
+
+// Statistics.defaultProps = {
+//   avatar: defaultImg,
+// };
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
 export default Statistics;
