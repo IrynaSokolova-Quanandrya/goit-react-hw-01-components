5d5bdbfad5710bda0f3df0693002c559f1ac9610
Iryna <irasokolova25@ukr.net>
2021-10-28 23:55:55 +0300

    add styles

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,18 +1,28 @@
 import FriendListItem from "./FriendListItem";
+import styled from "styled-components";
+const Styles = styled.div`
+  .friend-list {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
 console.log(FriendListItem);
 
 function FriendList({ friends }) {
   return (
-    <ul className="friend-list">
-      {friends.map((friend) => (
-        <FriendListItem
-          key={friend.id}
-          avatar={friend.avatar}
-          status={friend.isOnline}
-          name={friend.name}
-        />
-      ))}
-    </ul>
+    <Styles>
+      <ul className="friend-list">
+        {friends.map((friend) => (
+          <FriendListItem
+            key={friend.id}
+            avatar={friend.avatar}
+            status={friend.isOnline}
+            name={friend.name}
+          />
+        ))}
+      </ul>
+    </Styles>
   );
 }
 
