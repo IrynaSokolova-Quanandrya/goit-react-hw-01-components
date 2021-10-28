e6a4144e1cd69d4321ca29c4df7003ee60f2f38f
Iryna <irasokolova25@ukr.net>
2021-10-28 23:56:53 +0300

    add react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // import "./App.css";
 // import Profile from "./components/Profile/Profile";
 // import user from "./components/Profile/profile-user.json";
-import Statistics from "./components/Statistics/Statistics";
-import statisticalData from "./components/Statistics/statistical-data.json";
-// import FriendList from "./components/FriendList/FriendList";
-// import friends from "./components/FriendList/friends.json";
+// import Statistics from "./components/Statistics/Statistics";
+// import statisticalData from "./components/Statistics/statistical-data.json";
+import FriendList from "./components/FriendList/FriendList";
+import friends from "./components/FriendList/friends.json";
 // import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 // import transactions from "./components/TransactionHistory/transactions.json";
 
@@ -20,9 +20,9 @@ export default function App() {
         avatar={user.avatar}
         stats={user.stats}
       /> */}
-      <Statistics title="Upload stats" stats={statisticalData} />;
-      <Statistics stats={statisticalData} />;
-      {/* <FriendList friends={friends} />, */}
+      {/* <Statistics title="Upload stats" stats={statisticalData} />;
+      <Statistics stats={statisticalData} />; */}
+      <FriendList friends={friends} />,
       {/* <TransactionHistory items={transactions} />; */}
     </div>
   );
